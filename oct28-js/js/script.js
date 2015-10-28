aa85314a5c5b68e183e7758eba88870a5c95b300
Matthew Higley <matthewhigley@Matthews-MacBook-Pro.local>
2015-10-27 22:17:12 -0700

    adding portfolio

---
+++ oct28-js/js/script.js
@@ -0,0 +1,15 @@
+/* js goes here */
+
+var num1 = 50,
+	num2 = 100,
+	num3 = 2,
+	num4 = 16,
+	num5 = 5;
+
+console.log(num1 + num2 / num3);
+console.log((num1 + num2) / num3);
+console.log(num4 % num5);
+
+
+
+
