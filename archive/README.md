c7b0c386e30aa80021610dbb41443f89632299ed
Matthew Higley <matthewhigley@Matthews-MacBook-Pro.local>
2015-11-20 09:50:13 -0800

    updating portfolio

---
+++ archive/README.md
@@ -0,0 +1,4 @@
+# edc project folder
+Project folder for all edc work
+
+*All works for educational uses only. Logos and images are registered trademarks of their respective companies.
--- js/temp.js
+++
@@ -1,7 +0,0 @@
-var $btn = document.getElementsByTagName('button')[0],
-	$img = document.getElementById('bg-img');
-$btn.addEventListener('click', function(){
-	$img.classList.toggle();
-});
-
-
